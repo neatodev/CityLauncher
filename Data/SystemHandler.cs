eb0e15cb1c8c58ff6d14c648366c21e13ba75da3
neatodev <49599979+neatodev@users.noreply.github.com>
2022-09-26 01:13:41 +0200

    Change GPU read behavior

--- Data/SystemHandler.cs
+++ Data/SystemHandler.cs
@@ -31,23 +31,31 @@ namespace CityLauncher
         private string InitializeGPUValues()
         {
             RegDirectory = Path.Combine(Registry.LocalMachine.ToString(), "SYSTEM\\ControlSet001\\Control\\Class\\{4d36e968-e325-11ce-bfc1-08002be10318}\\0000");
+            var key = Registry.LocalMachine.OpenSubKey(@"SYSTEM\\ControlSet001\\Control\\Class\\{4d36e968-e325-11ce-bfc1-08002be10318}\\0000");
+
+            if (key == null)
+            {
+                RegDirectory = Path.Combine(Registry.LocalMachine.ToString(), "SYSTEM\\ControlSet001\\Control\\Class\\{4d36e968-e325-11ce-bfc1-08002be10318}\\0001");
+            }
 
             Nlog.Info("InitializeGPUValues - Recognized GPU as {0} with a total VRAM amount of {1}.", (string)Registry.GetValue(RegDirectory, "DriverDesc", "Could not find GPU name."), ConvertVRamValue((Int64)Registry.GetValue(RegDirectory, "HardwareInformation.qwMemorySize", 0)));
+            return (string)Registry.GetValue(RegDirectory, "DriverDesc", "Not found") + " (" + ConvertVRamValue((Object)Registry.GetValue(RegDirectory, "HardwareInformation.qwMemorySize", 0)) + ")";
 
-            return (string)Registry.GetValue(RegDirectory, "DriverDesc", "Not found") + " (" + ConvertVRamValue((Int64)Registry.GetValue(RegDirectory, "HardwareInformation.qwMemorySize", 0)) + ")";
         }
 
         ///<Returns VRAM value in GB in most cases.</Returns>.</summary>
-        private string ConvertVRamValue(Int64 VRam)
+        private string ConvertVRamValue(Object VRam)
         {
+            Int64 VRamValue = (Int64)VRam;
+
             var Affix = "MB";
-            if (VRam >= 1073741824)
+            if (VRamValue >= 1073741824)
             {
-                VRam = VRam / 1024;
+                VRamValue /= 1024;
                 Affix = "GB";
             }
-            VRam = VRam / 1048576;
-            return VRam.ToString() + Affix;
+            VRamValue = VRamValue / 1048576;
+            return VRamValue.ToString() + Affix;
         }
     }
 }
