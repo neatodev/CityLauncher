bd30e607f08d93eadf4f9f44d97ec5ac7769caa3
neatodev <49599979+neatodev@users.noreply.github.com>
2022-09-26 20:06:27 +0200

    Simplify expressions

--- Data/SystemHandler.cs
+++ Data/SystemHandler.cs
@@ -38,13 +38,13 @@ namespace CityLauncher
                 RegDirectory = Path.Combine(Registry.LocalMachine.ToString(), "SYSTEM\\ControlSet001\\Control\\Class\\{4d36e968-e325-11ce-bfc1-08002be10318}\\0001");
             }
 
-            Nlog.Info("InitializeGPUValues - Recognized GPU as {0} with a total VRAM amount of {1}.", (string)Registry.GetValue(RegDirectory, "DriverDesc", "Could not find GPU name."), ConvertVRamValue((Object)Registry.GetValue(RegDirectory, "HardwareInformation.qwMemorySize", 0)));
-            return (string)Registry.GetValue(RegDirectory, "DriverDesc", "GPU not found.") + " " + ConvertVRamValue((Object)Registry.GetValue(RegDirectory, "HardwareInformation.qwMemorySize", 0));
+            Nlog.Info("InitializeGPUValues - Recognized GPU as {0} with a total VRAM amount of {1}.", (string)Registry.GetValue(RegDirectory, "DriverDesc", "Could not find GPU name."), ConvertVRamValue((object)Registry.GetValue(RegDirectory, "HardwareInformation.qwMemorySize", 0)));
+            return (string)Registry.GetValue(RegDirectory, "DriverDesc", "GPU not found.") + " " + ConvertVRamValue((object)Registry.GetValue(RegDirectory, "HardwareInformation.qwMemorySize", 0));
 
         }
 
         ///<Returns VRAM value in GB in most cases.</Returns>.</summary>
-        private string ConvertVRamValue(Object VRam)
+        private string ConvertVRamValue(object VRam)
         {
             try
             {
