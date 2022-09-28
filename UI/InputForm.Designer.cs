005b86f55b1e9f1d631f2c4e3758ad9898628c7e
neatodev <49599979+neatodev@users.noreply.github.com>
2022-09-28 16:12:44 +0200

    Add correct icon to InputForm

--- UI/InputForm.Designer.cs
+++ UI/InputForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InputForm));
             this.label2 = new System.Windows.Forms.Label();
             this.KeybindValueLabel = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -103,6 +104,7 @@
             this.Controls.Add(this.KeybindValueLabel);
             this.Controls.Add(this.label2);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "InputForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "InputForm";
