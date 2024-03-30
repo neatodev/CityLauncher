34f45832f9fa32250de9396744e73c0ca47e6a4d
Neato <49599979+neatodev@users.noreply.github.com>
2024-03-30 03:40:36 +0100

    fix age old bug

--- UI/CityLauncher.cs
+++ UI/CityLauncher.cs
@@ -119,19 +119,19 @@ namespace CityLauncher
         private void MouseSensitivityTrackbar_Scroll(object sender, EventArgs e)
         {
             MouseSensitivityValueLabel.Text = MouseSensitivityTrackbar.Value.ToString();
-            DisplaySettingChanged = true;
+            ControlSettingChanged = true;
         }
 
         private void CustomFoV1Trackbar_Scroll(object sender, EventArgs e)
         {
             CustomFoV1ValueLabel.Text = CustomFoV1Trackbar.Value.ToString();
-            DisplaySettingChanged = true;
+            ControlSettingChanged = true;
         }
 
         private void CustomFoV2Trackbar_Scroll(object sender, EventArgs e)
         {
             CustomFoV2ValueLabel.Text = CustomFoV2Trackbar.Value.ToString();
-            DisplaySettingChanged = true;
+            ControlSettingChanged = true;
         }
 
         private void FwButton1_Click(object sender, EventArgs e)
